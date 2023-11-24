b3b49f3496a5d17a010fb86ce0f6dab2d93451c1
Josue Tapia <josuetapia2403@gmail.com>
2023-11-24 16:33:49 -0400

    segundo comit

--- src/app/modules/shareholders/components/crud-page/crud-page.component.ts
+++ src/app/modules/shareholders/components/crud-page/crud-page.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-crud-page',
@@ -6,5 +11,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./crud-page.component.css']
 })
 export class CrudPageComponent {
+public crudForm: FormGroup = this.fb.group({
+paternal_surname:['', [Validators.required, Validators.minLength(3) ]],
+maternal_surname:['', [ Validators.required, Validators.minLength(3) ]],
+name:['', [Validators.required, Validators.minLength(3) ]],
+
+})
+
+constructor(private fb: FormBuilder){}
+
+onSave(): void {
+  if (this.crudForm.invalid){return;}
+  console.log(this.crudForm.value)
+}
 
 }
--- src/app/modules/shareholders/shareholders.module.ts
+++ src/app/modules/shareholders/shareholders.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzInputModule } from 'ng-zorro-antd/input';
@@ -8,31 +9,32 @@ import { NzTableModule } from 'ng-zorro-antd/table';
 
 import { CrudPageComponent } from './components/crud-page/crud-page.component';
 import {
+  NewCrudPageComponent,
+} from './components/new-crud-page/new-crud-page.component';
+import {
   TablePageComponent,
 } from './components/table-page/table-page.component';
 import {
   ShareholdersPageComponent,
 } from './pages/shareholders-page/shareholders-page.component';
-import { NewCrudPageComponent } from './components/new-crud-page/new-crud-page.component';
 
 @NgModule({
   declarations: [
-  ShareholdersPageComponent,
+    ShareholdersPageComponent,
 
-  CrudPageComponent,
-  TablePageComponent,
-  NewCrudPageComponent,
+    CrudPageComponent,
+    TablePageComponent,
+    NewCrudPageComponent,
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
 
     NzTableModule,
     NzInputModule,
     NzMenuModule,
-    NzIconModule
+    NzIconModule,
   ],
-  exports:[
-
-    ],
+  exports: [],
 })
-export class ShareholdersModule { }
+export class ShareholdersModule {}
